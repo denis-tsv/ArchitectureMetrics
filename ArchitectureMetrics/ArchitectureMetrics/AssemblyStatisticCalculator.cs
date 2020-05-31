e5020084b53a6cf079deec122e7524989e5d6a4d
Denis Tsvettsikh <denis_tsvettsikh@epam.com>
2020-05-31 14:55:58 +0700

    Fixed calculator

--- ArchitectureMetrics/ArchitectureMetrics/AssemblyStatisticCalculator.cs
+++ ArchitectureMetrics/ArchitectureMetrics/AssemblyStatisticCalculator.cs
@@ -8,27 +8,34 @@ using CsvHelper;
 namespace ArchitectureMetrics
 {
 	//var path = Path.Combine(Directory.GetCurrentDirectory(), @"bin\Debug\netcoreapp2.1");
-	//new AssemblyStatisticCalculator().Calculate(path, new [] { "TCO.TEARECS"}, new []{"Domain", "DataAccess.Entities"});
+	//new AssemblyStatisticCalculator().Calculate(path, "TCO.TEARECS", x => false);
 
 	//var path = Path.Combine(Directory.GetCurrentDirectory(), @"bin\Debug\netcoreapp3.0");
-	//new AssemblyStatisticCalculator().Calculate(path, "ContestantRegister", "Entities");
-
-	//var path = Path.Combine(Directory.GetCurrentDirectory(), @"bin\Debug\netcoreapp3.1");
-	//new AssemblyStatisticCalculator().Calculate(path, "Max", "Entities");
-
+	//Func<Type, bool> dtoCheck = (x) =>
+	//{
+	//if (AssemblyStatisticCalculator.IsAnonymousType(x)) return false;
+	//if (!x.FullName.Contains("Cqrs")) return false;
+	//if (x.Name.EndsWith("ViewModel") || x.Name.EndsWith("Query") || x.Name.EndsWith("Command")) return true;
+	//return AssemblyStatisticCalculator.IsPocoType(x);
+	//};
+	//new AssemblyStatisticCalculator().Calculate(path, "ContestantRegister", dtoCheck);
+    
+    //var path = Path.Combine(Directory.GetCurrentDirectory(), @"bin\Debug\netcoreapp3.1");
+    //new AssemblyStatisticCalculator().Calculate(path, "Max", x => x.Name.EndsWith("Dto"));
+	
 	//var path = @"c:\Users\dtjd\Desktop\PravoToDenis_no_git_hub\Sites\CaseMap\bin\";
-    //new AssemblyStatisticCalculator().Calculate(path, new []{ "CaseMap", "CaseDotStar" }, new string[] {}, @"c:\Users\dtjd\Desktop\PravoRu.csv");
+	//new AssemblyStatisticCalculator().Calculate(path, new []{ "CaseMap", "CaseDotStar" }, null, @"c:\Users\dtjd\Desktop\Stat.csv");
 
 	public class AssemblyStatisticCalculator
 	{
 		private static readonly HashSet<string> BaseMethods = new HashSet<string> { "ToString", "Equals", "GetHashCode", "GetType" };
 
-        public void Calculate(string path, string assemblyNamePrefix, string entities, string outputFilePath = "AssemblyStatistic.csv")
+        public void Calculate(string path, string assemblyNamePrefix, Func<Type, bool> dtoCheckerFunc = null, string outputFilePath = "AssemblyStatistic.csv")
         {
-            Calculate(path, new[] {assemblyNamePrefix}, new[] {entities}, outputFilePath);
+            Calculate(path, new[] {assemblyNamePrefix}, dtoCheckerFunc, outputFilePath);
         }
 
-		public void Calculate(string path, string[] assemblyNamePrefixes, string[] entities, string outputFilePath = "AssemblyStatistic.csv")
+		public void Calculate(string path, string[] assemblyNamePrefixes, Func<Type, bool> dtoCheckerFunc = null, string outputFilePath = "AssemblyStatistic.csv")
         {
             var assemblies = assemblyNamePrefixes.SelectMany(x => Directory.EnumerateFiles(path, $"{x}*dll"))
                 .Select(Assembly.LoadFile);
@@ -69,10 +76,14 @@ namespace ArchitectureMetrics
 				assemblyStat.Exceptions = types.Count(x => typeof(Exception).IsAssignableFrom(x));
 				assemblyStat.Events = types.Count(x => typeof(Delegate).IsAssignableFrom(x));
 				assemblyStat.EventArgs = types.Count(x => typeof(EventArgs).IsAssignableFrom(x));
-				assemblyStat.DTOs = types.Count(x => IsDtoType(x, entities));
+
+                if (dtoCheckerFunc != null)
+                {
+                    assemblyStat.DTOs = types.Count(dtoCheckerFunc);
+				}
 
 				//Not anonymous 
-				assemblyStat.TotalClasses = types.Count(x => !x.Name.Contains('_') && !x.Name.Contains('<') && !x.Name.Contains('>'));
+				assemblyStat.TotalClasses = types.Count(x => !IsAnonymousType(x));
 
 				stats.Add(assemblyStat);
 			}
@@ -90,13 +101,13 @@ namespace ArchitectureMetrics
 			}
 		}
 
-		private bool IsDtoType(Type type, string[] entitiesPrefixes)
+        public static bool IsAnonymousType(Type type)
         {
-            var assemblyName = type.Assembly.GetName().Name;
-			if (entitiesPrefixes != null && entitiesPrefixes.Any(x => assemblyName.Contains(x))) return false;
+            return type.Name.Contains('_') || type.Name.Contains('<') || type.Name.Contains('>');
+        }
 
-            if (type.Name.EndsWith("Dto")) return true;
-
+		public static bool IsPocoType(Type type)
+        {
 			if (!type.IsClass || type.IsAbstract) return false;
 			if (type.GetFields().Any() ||
 				type.GetMethods().Any(x => !x.Name.StartsWith("get_") && !x.Name.StartsWith("set_") && !BaseMethods.Contains(x.Name)) ||
